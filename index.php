8f2ea74b1f0cec2714cb12bf36743acc0e989edb
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-13 09:46:23 +0100

    ajout exo4

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
-<?php
-$numberOne = 1;
-$result = $numberOne / 2;
-while (($numberOne + $result) <=10) {
-  $result = $numberOne / 2;
-  $numberOne = $numberOne + $result;
-  echo $numberOne.'<br/>';
-}
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 4 partie 7 php</title>
+  </head>
+  <body>
+    <form class="form" action="user.php" method="post">
+      <input type="lastname" name="lastname" placeholder="Nom">
+      <input type="firstname" name="firstname" placeholder="Prénom">
+      <button type="submit" name="validate">Valider</button>
+    </form>
+    <?php
+
+    ?>
+  </body>
+</html>
