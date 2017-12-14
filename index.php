29411b5a6122ab0a75e745efb7602124cfdd049e
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-14 08:47:13 +0100

    ajout exo4

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>exercice 4 partie 7 php</title>
-  </head>
-  <body>
-    <form class="form" action="user.php" method="post">
-      <input type="lastname" name="lastname" placeholder="Nom">
-      <input type="firstname" name="firstname" placeholder="Prénom">
-      <button type="submit" name="validate">Valider</button>
-    </form>
-    <?php
-
-    ?>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <title>exercice 4 partie 8 php</title>
+</head>
+<body>
+  <p>
+  <?php
+  echo($_COOKIE['pseudo'] . ' ' . $_COOKIE['password']);
+  ?>
+  </p>
+</body>
 </html>
